dfa72b1b0badf928da66eb901c5e9d16cd09fcac
“Archana” <archana.villalba@gmail.com>
2018-12-13 18:49:01 +0100

    wip

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,66 @@
 const express = require('express');
 const router  = express.Router();
+const Book = require('../models/book.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+
+
+router.get('/books', (req, res, next) => {
+  Book.find()
+    .then(books => {
+      res.render("books", { books });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+router.get('/book/:id', (req, res, next) => {  //pour afficher le detail par livre dans book-detail
+  let bookId = req.params.id;
+  Book.findOne({'_id': bookId})
+    .then(book => {
+      res.render("book-detail", { book })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+router.post('/books/add', (req, res, next) => { //pour prendre les valeurs du formulaires pour les ajouter à notre bdd mongo
+  const { title, author, description, rating } = req.body; //obligatoire pour passer ensuite les valeurs à Book
+  const newBook = new Book({ title, author, description, rating})
+  newBook.save()
+  .then((book) => {
+    res.redirect('/books');
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+});
+
+router.get('/books/edit', (req, res, next) => {
+  Book.findOne({_id: req.query.book_id})
+  .then((book) => {
+    res.render("book-edit", {book});
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+});
+
+router.post('/books/edit', (req, res, next) => {
+  const { book_id,title, author, description, rating } = req.body;
+  Book.update({_id: book_id}, { $set: {title, author, description, rating }})
+  .then((book) => {
+    res.redirect('/books');
+  })
+  .catch((error) => {
+    console.log(error);
+  })
 });
 
 module.exports = router;
